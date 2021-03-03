ff308d6da1fb3ee6401bffe7b57087ce8e6cdd08
Erika Tavera <dugarerika@gmail.com>
2021-03-03 16:54:28 +0300

    ajustando el layout con antd

--- src/componentes/auth/Signin.js
+++ src/componentes/auth/Signin.js
@@ -1,13 +1,21 @@
-import { Form, Input, Button, Checkbox } from 'antd';
+import {
+	Form,
+	Input,
+	Typography,
+	Button,
+	Checkbox
+} from 'antd';
+
 import React, { useState } from 'react';
 
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
-import { makeStyles } from '@material-ui/core/styles';
+
 import auth from './../auth/auth-helper';
 import { Redirect } from 'react-router-dom';
 import { signin } from '../../API/api-auth.js';
+import Base from '../layout/Base.js';
+
+const { Title } = Typography;
 
 const layout = {
 	labelCol: {
@@ -24,34 +32,7 @@ const tailLayout = {
 	}
 };
 
-const useStyles = makeStyles((theme) => ({
-	card: {
-		maxWidth: 600,
-		margin: 'auto',
-		textAlign: 'center',
-		marginTop: theme.spacing(5),
-		paddingBottom: theme.spacing(2)
-	},
-	error: {
-		verticalAlign: 'middle'
-	},
-	title: {
-		marginTop: theme.spacing(2),
-		color: theme.palette.openTitle
-	},
-	textField: {
-		marginLeft: theme.spacing(1),
-		marginRight: theme.spacing(1),
-		width: 300
-	},
-	submit: {
-		margin: 'auto',
-		marginBottom: theme.spacing(2)
-	}
-}));
-
 export default function Signin(props) {
-	const classes = useStyles();
 	const [
 		values,
 		setValues
@@ -99,50 +80,43 @@ export default function Signin(props) {
 	}
 
 	return (
-		<Form {...layout} className={classes.card}>
-			auth/Signin
-			<Form.Item>
-				<Typography variant='h5' className={classes.title}>
-					Entrar
-				</Typography>
-				<TextField
+		<Base>
+			<Form {...layout}>
+				<Title strong>Entrar</Title>
+				<Form.Item
 					id='email'
-					type='email'
+					name='email'
 					label='Email'
-					className={classes.textField}
 					value={values.email}
-					onChange={handleChange('email')}
-					margin='normal'
-				/>
-				<br />
-				<TextField
+					onChange={handleChange('email')}>
+					<Input />
+				</Form.Item>
+
+				<Form.Item
 					id='password'
-					type='password'
+					name='password'
 					label='Password'
-					className={classes.textField}
 					value={values.password}
-					onChange={handleChange('password')}
-					margin='normal'
-				/>
-				<br />{' '}
-				{values.error && (
-					<Typography component='p' color='error'>
-						<Icon color='error' className={classes.error}>
-							error
-						</Icon>
-						{values.error}
-					</Typography>
-				)}
-			</Form.Item>
-			<Form.Item {...tailLayout}>
-				<Button
-					type='primary'
-					htmlType='submit'
-					onClick={clickSubmit}
-					className={classes.submit}>
-					Enviar
-				</Button>
-			</Form.Item>
-		</Form>
+					onChange={handleChange('password')}>
+					<br />{' '}
+					{values.error && (
+						<Typography component='p' color='error'>
+							<Icon color='error'>error</Icon>
+							{values.error}
+						</Typography>
+					)}
+					<Input.Password />
+				</Form.Item>
+
+				<Form.Item {...tailLayout}>
+					<Button
+						type='primary'
+						htmlType='submit'
+						onClick={clickSubmit}>
+						Enviar
+					</Button>
+				</Form.Item>
+			</Form>
+		</Base>
 	);
 }
