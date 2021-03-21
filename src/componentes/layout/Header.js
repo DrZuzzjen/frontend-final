11ebcfdd30d7fd15883862d973e2f54568585b3c
Francois Gutierrez <gutijeanf@gmail.com>
2021-03-21 22:55:23 +0100

    Small update

--- src/componentes/layout/Header.js
+++ src/componentes/layout/Header.js
@@ -165,7 +165,8 @@ export default function Header() {
 			<Row className='navbar'>
 				<Space>
 					<Col flex='auto'>
-<<<<<<< HEAD
+
+
 						<a href='#' onClick={handleHome}>
 							<Image
 								width={130}
@@ -173,11 +174,9 @@ export default function Header() {
 								flex='auto'
 								preview={false}
 							/>
-=======
+
 						<a href='' onClick={handleHome}> 
-						<Image width={130} src='./wallarock.logo.svg' flex='auto' preview={false}/>
->>>>>>> Small update
-						</a>
+						<Image width={130} src='./wallarock.logo.svg' flex='auto' preview={false}/></a>
 					</Col>
 					<Col flex='auto'>
 						{' '}
