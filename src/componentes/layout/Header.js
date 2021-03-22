d5233fb69cae3c4c9a0f33921023d207e326f645
Francois Gutierrez <gutijeanf@gmail.com>
2021-03-22 22:50:10 +0100

    locked out

--- src/componentes/layout/Header.js
+++ src/componentes/layout/Header.js
@@ -164,8 +164,6 @@ export default function Header() {
 			<Row className='navbar'>
 				<Space>
 					<Col flex='auto'>
-
-
 						<a href='#' onClick={handleHome}>
 							<Image
 								width={130}
@@ -173,9 +171,7 @@ export default function Header() {
 								flex='auto'
 								preview={false}
 							/>
-
-						<a href='' onClick={handleHome}> 
-						<Image width={130} src='./wallarock.logo.svg' flex='auto' preview={false}/></a>
+						</a>
 					</Col>
 					<Col flex='auto'>
 						{' '}
