9a6adc1d9d69bd24034637a080f9bdc47e931722
Erika Tavera <dugarerika@gmail.com>
2021-03-23 20:48:05 +0300

    modifico direccion del servidor

--- src/API/api-config.js
+++ src/API/api-config.js
@@ -1,9 +1,10 @@
-export const API_ROOT = `http://34.244.177.216:4000`;
+export const API_ROOT = `http://localhost:3000`;
 
 const config = {
-  stripe_connect_test_client_id: "YOUR_stripe_connect_test_client",
-  stripe_test_secret_key: "YOUR_stripe_test_secret_key",
-  stripe_test_api_key: "YOUR_stripe_test_api_key",
+	stripe_connect_test_client_id:
+		'YOUR_stripe_connect_test_client',
+	stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
+	stripe_test_api_key: 'YOUR_stripe_test_api_key'
 };
 
 export default config;
